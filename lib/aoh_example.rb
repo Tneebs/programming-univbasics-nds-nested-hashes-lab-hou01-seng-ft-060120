204ba3e8ee0736deedcba69c0fbdfc92482ade43
Ted Neben <tedneben@gmail.com>
2020-05-19 04:09:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,11 +14,7 @@ end
 
 def literal_aoh
   array_of_hashes = [{
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    
   }]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
